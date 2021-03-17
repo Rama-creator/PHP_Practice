042bc9adbe13ce0318c790e436a040adcbe175d0
Sujud Ramadhan <62531850+Rama-creator@users.noreply.github.com>
2021-03-17 23:11:26 +0700

    Add files via upload

---
+++ Exercises.php
@@ -0,0 +1,17 @@
+<?php 
+	echo "Nomor 1";
+	echo "<br>";
+	$ikanku = array("Teri","Lele","Bawal","Tuna");
+
+	echo "Ikan $ikanku[0] sering dijumpai di nasi kucing angkringan dan saya konsumsi pas tanggal tua. Kalo tanggal muda sering kuliner $ikanku[1], $ikanku[2], $ikanku[3].";
+
+	echo "<br>";
+	echo "<br>";
+	
+	echo "Nomor 2";
+	echo "<br>";
+    echo date('d-m-Y', strtotime('2017-03-14'));
+    
+    //Sujud Ramadhan
+    //12191685
+?>
